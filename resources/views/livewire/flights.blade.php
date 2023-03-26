b2a11f90b9bfb774d304dd15f4aa5a9664418414
johnsonDevops <59789312+johnsonDevops@users.noreply.github.com>
2023-03-26 17:54:28 -0400

    updated hidden updated at row on small in flights

--- resources/views/livewire/flights.blade.php
+++ resources/views/livewire/flights.blade.php
@@ -42,7 +42,7 @@
                                     <thead class="border-b font-medium bg-slate-100">
                                         <tr>
                                             <th scope="col" class="px-6 py-4">Itinerary Name</th>
-                                            <th scope="col" class="px-6 py-4">Last Updated</th>
+                                            <th scope="col" class="px-6 py-4 hidden md:block">Last Updated</th>
                                             <th scope="col" class="px-6 py-4">Actions</th>
                                         </tr>
                                     </thead>
@@ -51,9 +51,11 @@
                                         @foreach ($users->flights->where('is_active', 1) as $flight)
                                                 <tr class="border-b">
                                                     <td class="px-6 py-4 font-medium">{{ $flight->flt_desc ?? 'Flight Name' }}</td>
-                                                    <td class="px-6 py-4">{{ $flight->updated_at ?? 'Flight Name' }}</td>
-                                                    <td class="px-6 py-4">
 
+                                                    <td class="px-6 py-4 hidden md:block">{{ $flight->updated_at ?? 'Flight Name' }}</td>
+
+
+                                                    <td class="px-6 py-4">
                                                         <a href="{{ asset( 'storage/' . $flight->flt_file ) ?? ''}}" class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded inline-flex items-center" download>
                                                             <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
                                                             <span>Download</span>
