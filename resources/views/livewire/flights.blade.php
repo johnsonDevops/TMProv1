ee9578b241d137519111c46c178f06ce733412c6
johnsonDevops <59789312+johnsonDevops@users.noreply.github.com>
2023-03-26 15:06:57 -0400

    download Button

--- resources/views/livewire/flights.blade.php
+++ resources/views/livewire/flights.blade.php
@@ -53,6 +53,13 @@
                                                     <td class="px-6 py-4 font-medium">{{ $flight->flt_desc ?? 'Flight Name' }}</td>
                                                     <td class="px-6 py-4">{{ $flight->updated_at ?? 'Flight Name' }}</td>
                                                     <td class="px-6 py-4">
+
+                                                        <a href="{{ asset( 'storage/' . $flight->flt_file ) ?? ''}}" class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded inline-flex items-center" download>
+                                                            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                                                            <span>Download</span>
+                                                        </a>
+
+                                                        {{-- 
                                                         <a href="{{ asset( 'storage/' . $flight->flt_file ) ?? ''}}" type="button"
                                                             class="md:min-w-max mb-2 md:mb-0 px-6 py-2 md:mr-2 text-xs font-medium text-center text-white bg-indigo-500 rounded-lg hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-indigo-300"
                                                             target="_blank">
@@ -63,7 +70,8 @@
                                                             class="md:min-w-max mb-2 md:mb-0  px-3 py-2 text-xs font-medium text-center text-white bg-indigo-500 rounded-lg hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-indigo-300"
                                                             download>
                                                             Downlaod
-                                                        </a>
+                                                        </a> --}}
+
                                                     </td>
                                                 </tr>
                                             @endforeach
